e626d1dd2b30215f6fa46c17c43fd47db48063dc
Anthony <alam6@stuy.edu>
2018-09-28 11:43:24 -0400

    added defintions for length and strcpy

---
+++ string.c
@@ -0,0 +1,29 @@
+#include <strings.h>
+#include <stdio.h>
+
+int length(char* s){
+  int length =0;
+  while (*s && s++){
+    length++;
+  }
+  return length;
+}
+
+char* strcpy(char* dest, char* source){
+  char* original = dest;
+  while (*source){
+    *dest=*source;
+    source++;
+    dest++;
+  }
+  return original;
+}
+
+int main(){
+  char new[10]="hi";
+  char src[]= "src";
+  strcpy(new,src);
+  printf("%s\n", new);
+  printf("%d \n", length("hello"));
+  return 0;
+}
